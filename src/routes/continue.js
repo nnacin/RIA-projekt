50509a1308d83f27021559e65cdac5f15cf9a278
natnat <tomorrow_is_waiting_for_your_smile_@hotmail.com>
2016-01-23 19:18:20 +0000

    More work on ordering

---
+++ src/routes/continue.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const creds = require('../../creds');
+const Promise = require('bluebird')
+const ad = require('../adapter');
+const Adapter = new ad();
+const getAllPizza = Promise.promisify(Adapter.getAllPizza);
+const getAllDrink = Promise.promisify(Adapter.getAllDrink);
+const debug = require('debug')('staff/index');
+
+/* GET users listing. */
+router.post('/continue', isLoggedIn , function (req, res)  {
+  res.render('location');
+});
+
+
+
+
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/login');
+}
+
+module.exports = router;
---
+++ src/routes/drinks.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const creds = require('../../creds');
+const Promise = require('bluebird')
+const ad = require('../adapter');
+const Adapter = new ad();
+const getAllDrink = Promise.promisify(Adapter.getAllDrink);
+const debug = require('debug')('menuDrinks');
+
+
+router.get('/drinks', (req, res, next) => {
+  getAllDrink()
+  .then(r => {
+    res.render('menuDdrinks', { drink: r });
+  })
+});
+
+/* GET users listing. */
+router.post('/drinks', (req, res, next) => {
+    var ids = req.body['id'];
+    var quantities = req.body['quantity'];
+    var pizzas = [];
+    ids.forEach(function (id){
+       pizzas.push({id: id});
+    });
+
+    getAllDrink()
+        .then(r => {
+        res.render('menuDrinks', { drink: r });
+    })
+});
+
+module.exports = router;
