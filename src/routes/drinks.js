a6f3591ee3117ba1dc3dbfdedee5dd40c8c7f546
imilako <inoven@gmail.com>
2016-01-29 07:04:09 +0100

    Order pizza mostly done

--- src/routes/drinks.js
+++ src/routes/drinks.js
@@ -6,29 +6,30 @@ const Promise = require('bluebird')
 const ad = require('../adapter');
 const Adapter = new ad();
 const getAllDrink = Promise.promisify(Adapter.getAllDrink);
-const debug = require('debug')('menuDrinks');
+const debug = require('debug')('pizzamins:menuDrinks');
 
 
 router.get('/drinks', (req, res, next) => {
+  let order = req.session.order;
+  if (!order) return res.redirect('menu');
+  if (!menu.pizzas) return res.redirect('menu');
   getAllDrink()
   .then(r => {
-    res.render('menuDdrinks', { drink: r });
+    res.render('menuDrinks', { drink: r });
   })
 });
 
-/* GET users listing. */
 router.post('/drinks', (req, res, next) => {
-    //var ids = req.body['id'];
-    //var quantities = req.body['quantity'];
-    //var pizzas = [];
-    /*ids.forEach(function (id){
-       pizzas.push({id: id});
-    });*/
+  let menu = req.body;
+  if (!menu) return res.redirect('menu');
+  if (!menu.pizzas) return res.redirect('menu');
 
-    getAllDrink()
-        .then(r => {
-        res.render('menuDrinks', { drink: r });
-    })
+  let order = req.session.order;
+  if (!order) req.session.order = {};
+  order.pizzas = menu.pizzas;
+  debug(`req.session ${req.session.order}`)
+  debug(`order ${order}`)
+  return res.redirect(drinks);
 });
 
 module.exports = router;
