c66b0db362b732eb4b52a316ae911cd4303355db
natnat <tomorrow_is_waiting_for_your_smile_@hotmail.com>
2016-01-30 09:55:43 +0000

    Style change to input for datetimepicker and check if object if empty (session.order) not only if it exists

--- src/routes/reditectHandler.js
+++ src/routes/reditectHandler.js
@@ -5,9 +5,10 @@ const debug = require('debug')('redirectHandler');
 
 router.get('/redirectHandler' , function (req, res)  {
     let order = req.session.order;
-    if (order && req.isAuthenticated())
-        res.redirect('dispatch');
-    else
+    if (order && req.isAuthenticated()) {
+        if (Object.keys(order).length)
+            res.redirect('dispatch');
+    } else
         res.redirect('/');
 });
 
